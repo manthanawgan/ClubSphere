bd4bbc46515af2c00bfa5b5b514e61bebad83e80
Manthan Awgan <manthanawgan@gmail.com>
2025-03-19 23:17:58 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import EventsShowcase from './components/EventsShowcase'
+import DescriptionSection from './components/DescriptionSection'
 import './App.css';
 
 function App() {
@@ -10,6 +11,8 @@ function App() {
       <Header />
       <Hero />
       <EventsShowcase />
+      <DescriptionSection />
+      
       {/* Add other landing page sections here later */}
     </div>
   );
